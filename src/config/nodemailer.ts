2d4310439a62bae744c27d0abec5d6832dcaa642
Roman Yaresko <iam.roman.yaresko@gmail.com>
2026-06-27 17:50:30 +0200

    Fix: nodemailer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,5 +1,8 @@
 import * as nodemailer from "nodemailer";
+import { setDefaultResultOrder } from "node:dns";
 import { env } from "@/config/env";
+
+setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
@@ -9,7 +12,7 @@ export const transporter = nodemailer.createTransport({
     user: env.SMTP_USER,
     pass: env.SMTP_PASS,
   },
-  connectionTimeout: 10_000,
-  greetingTimeout: 10_000,
-  socketTimeout: 15_000,
+  connectionTimeout: 60_000,
+  greetingTimeout: 60_000,
+  socketTimeout: 60_000,
 });
